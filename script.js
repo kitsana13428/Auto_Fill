3d9d9d29248f4e96176d68aedb9ee149d8f32d3c
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-09-19 20:56:25 +0700

    Finish!

---
+++ script.js
@@ -0,0 +1,7 @@
+
+$.Thailand({
+    $district: $('#district'), // input ของตำบล
+    $amphoe: $('#amphoe'), // input ของอำเภอ
+    $province: $('#province'), // input ของจังหวัด
+    $zipcode: $('#zipcode'), // input ของรหัสไปรษณีย์
+});
